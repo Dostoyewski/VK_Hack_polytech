5958bdc2e9dd855eca86c174efaaef81dcc94056
Dostoyewski <dostoyewski@yandex.ru>
2019-09-28 03:34:49 +0300

    changed site properties

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '0x!b#(1*cd73w$&azzc6p+essg7v=g80ls#z&xcx*mpemx&@9$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SITE_ID = 3
+SITE_ID = 4
 
 ALLOWED_HOSTS = ['*']
 
